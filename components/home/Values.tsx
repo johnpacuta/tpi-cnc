f5f6ec1a86a8100e2fa8ad32e9921cf1bc062ec7
John Pacuta <johnpacuta@gmail.com>
2025-10-30 16:06:03 -0400

    icons

--- components/home/Values.tsx
+++ components/home/Values.tsx
@@ -22,7 +22,7 @@ export function Values() {
 
                 <div className="space-y-6">
                     <div className="flex items-start space-x-4">
-                        <div className="bg-brand-blue p-2 rounded-full">
+                        <div className="p-2 rounded-full">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path
                                     d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"/>
@@ -36,7 +36,7 @@ export function Values() {
                     </div>
 
                     <div className="flex items-start space-x-4">
-                        <div className="bg-brand-blue p-2 rounded-full">
+                        <div className="p-2 rounded-full">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M5.5 16a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 16h-8z"/>
                             </svg>
@@ -49,7 +49,7 @@ export function Values() {
                     </div>
 
                     <div className="flex items-start space-x-4">
-                        <div className="bg-brand-blue p-2 rounded-full">
+                        <div className="p-2 rounded-full">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd"
                                       d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
@@ -64,7 +64,7 @@ export function Values() {
                     </div>
 
                     <div className="flex items-start space-x-4">
-                        <div className="bg-brand-blue p-2 rounded-full">
+                        <div className="p-2 rounded-full">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path
                                     d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"/>
