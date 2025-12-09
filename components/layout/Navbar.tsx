9c5bf519a2cb6ca2ffc02013f0830e9ea8cc3c22
John Pacuta <johnpacuta@gmail.com>
2025-12-09 10:37:24 -0500

    google tag update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { GoogleTagManager } from '@next/third-parties/google';
+
 import {Button} from "@/components/ui/button"
 import Link from "next/link"
 import {ChevronDown, Menu, Phone} from "lucide-react"
@@ -72,7 +74,7 @@ export function Navbar() {
 
     return (
         <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
-            {/* Top Bar */}
+            <GoogleTagManager gtmId="G-7385J6MX2L" />
             <div className="border-b">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
