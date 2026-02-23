c0224ba4cb3f796ece317b460c7bf7b89d8afe6a
johnp <johnpacuta@gmail.com>
2026-02-23 17:30:30 -0500

    corrected next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // Next.js supports this, but the TS type doesn't always include it yet.
-    outputFileTracingIncludes: {
-      "*": ["./node_modules/pdfkit/js/data/*.afm"],
-    },
-  } as any,
 };
 
 export default nextConfig;
