55c1ae198c614202433c9f9b6523cbdf940c545a
johnp <johnpacuta@gmail.com>
2026-03-10 23:16:32 -0400

    johnford

--- lib/news.ts
+++ lib/news.ts
@@ -16,7 +16,7 @@ export const NEWS_ARTICLES: NewsArticle[] = [
     imgSrc: "/images/Johnford-DMC-5axis.png",
     excerpt: "Johnford’s heavy casting, poured in their own foundry, delivers unsurpassed rigidity and longevity.",
     content:
-        "Johnford machines are used in shops that need dependable performance for large-part machining, heavy-duty cutting, and precision work. Their product range includes moving-table and fixed-table bridge mills, slant-bed lathes in multiple configurations, and vertical machining centers for mold, die, and general machining applications",
+        "Johnford machines are used in shops that need dependable performance for large-part machining, heavy-duty cutting, and precision work. Their product range includes moving-table and fixed-table bridge mills, slant-bed lathes in multiple configurations, and vertical machining centers for mold, die, and general machining applications.",
     youtubeVideoId: "Sa2uS6eU7n4?si=WdVsFT8f41G-q0SO"},
   {
     slug: "excetek",
